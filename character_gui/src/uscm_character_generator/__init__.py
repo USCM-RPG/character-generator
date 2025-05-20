3f9b415b7ce2f2d1b29d6d184118cbd7a1898a81
Viktor E.G. Bengtsson <viktor.e.g.bengtsson@gmail.com>
2025-05-20 23:15:32 +0200

    Replaced requirements.txt with pyproject.toml and added requirement for Python 3.12 or higher.

--- character_gui/src/uscm_character_generator/__init__.py
+++ character_gui/src/uscm_character_generator/__init__.py
@@ -1,1 +1,9 @@
-from character_gui import character_generator, extra_types
+from importlib.metadata import PackageNotFoundError, version
+
+from uscm_character_generator import character_generator, extra_types
+
+try:
+    __version__ = version("uscm-character-generator")
+except PackageNotFoundError:
+    # package is not installed
+    pass
